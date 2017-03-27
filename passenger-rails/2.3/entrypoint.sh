534d39dd00ff2803d09415ddb28992fa7797611b
Scott Barber <scott.barber@youearnedit.com>
2017-03-27 11:21:55 -0500

    Add passenger-rails 2.3 Dockerfile with deps

---
+++ passenger-rails/2.3/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Convert the ERB templates into real files
+for file in /usr/src/nginx/{,**/}*.erb; do
+  if [ -f "$file" ]; then
+    # don't overwrite an existing destination file
+    if [ ! -e "${file%.*}" ]; then
+      erb -T- "$file" > "${file%.*}"
+      echo "${file%.*}: generated."
+    else
+      >&2 echo "${file%.*}: SKIPPED! refusing to overwrite existing file."
+    fi
+  fi
+done
+
+# Now run nginx
+exec sudo -E /usr/sbin/nginx -c /usr/src/nginx/nginx.conf
