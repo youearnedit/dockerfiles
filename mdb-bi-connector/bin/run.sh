b220ffaabf114124ad68294982e5cd724cb6c39c
John Davis <john.davis@youearnedit.com>
2019-07-03 14:11:15 -0500

    Enable SSL to be turned on

--- mdb-bi-connector/bin/run.sh
+++ mdb-bi-connector/bin/run.sh
@@ -4,6 +4,6 @@ MONGO_URI="mongodb://${MONGO_HOST}:${MONGO_PORT}"
 service rsyslog start
 mkfifo mdb-bi-connector.log
 tail -f mdb-bi-connector.log&
-echo /opt/mongodb-bi-linux-x86_64-debian92-v${BI_CONNECTOR_VERSION}/bin/mongosqld --addr ${ADDR} --mongo-uri ${MONGO_URI} --auth --mongo-username ${SAMPLING_USER} --mongo-password ${SAMPLING_PASS} --logPath=mdb-bi-connector.log
-/opt/mongodb-bi-linux-x86_64-debian92-v${BI_CONNECTOR_VERSION}/bin/mongosqld --addr ${ADDR} --mongo-uri ${MONGO_URI} --auth --mongo-username ${SAMPLING_USER} --mongo-password ${SAMPLING_PASS} --logPath=mdb-bi-connector.log  --logAppend --logRotate reopen -vv
+echo /opt/mongodb-bi-linux-x86_64-debian92-v${BI_CONNECTOR_VERSION}/bin/mongosqld --sslMode ${SSL_MODE} --sslPemKeyFile ${SSL_KEY_FILE} --sslPEMKeyPassword ${SSL_KEY_PASS} --sslAllowInvalidCertificates --sslAllowInvalidHostnames --addr ${ADDR} --mongo-uri ${MONGO_URI} --auth --mongo-username ${SAMPLING_USER} --mongo-password ${SAMPLING_PASS} --logPath=mdb-bi-connector.log
+/opt/mongodb-bi-linux-x86_64-debian92-v${BI_CONNECTOR_VERSION}/bin/mongosqld --sslMode ${SSL_MODE} --sslPemKeyFile ${SSL_KEY_FILE} --sslPEMKeyPassword ${SSL_KEY_PASS} --sslAllowInvalidCertificates --sslAllowInvalidHostnames --addr ${ADDR} --mongo-uri ${MONGO_URI} --auth --mongo-username ${SAMPLING_USER} --mongo-password ${SAMPLING_PASS} --logPath=mdb-bi-connector.log  --logAppend --logRotate reopen -vv
 
