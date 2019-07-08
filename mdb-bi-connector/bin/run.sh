656aa1581315747192cf53a6acfa7e2ec3e19b1e
John Davis <john.davis@youearnedit.com>
2019-07-08 13:08:14 -0500

    Add certificate logic

--- mdb-bi-connector/bin/run.sh
+++ mdb-bi-connector/bin/run.sh
@@ -4,6 +4,10 @@ MONGO_URI="mongodb://${MONGO_HOST}:${MONGO_PORT}"
 service rsyslog start
 mkfifo mdb-bi-connector.log
 tail -f mdb-bi-connector.log&
-echo /opt/mongodb-bi-linux-x86_64-debian92-v${BI_CONNECTOR_VERSION}/bin/mongosqld --sslMode ${SSL_MODE} --sslPemKeyFile ${SSL_KEY_FILE} --sslPEMKeyPassword ${SSL_KEY_PASS} --sslAllowInvalidCertificates --sslAllowInvalidHostnames --addr ${ADDR} --mongo-uri ${MONGO_URI} --auth --mongo-username ${SAMPLING_USER} --mongo-password ${SAMPLING_PASS} --logPath=mdb-bi-connector.log
-/opt/mongodb-bi-linux-x86_64-debian92-v${BI_CONNECTOR_VERSION}/bin/mongosqld --sslMode ${SSL_MODE} --sslPemKeyFile ${SSL_KEY_FILE} --sslPEMKeyPassword ${SSL_KEY_PASS} --sslAllowInvalidCertificates --sslAllowInvalidHostnames --addr ${ADDR} --mongo-uri ${MONGO_URI} --auth --mongo-username ${SAMPLING_USER} --mongo-password ${SAMPLING_PASS} --logPath=mdb-bi-connector.log  --logAppend --logRotate reopen -vv
+#Concatenate the cert and key together
+cat ${SSL_KEY_FILE} >> /combined-certs/combined.pem
+cat ${SSL_CERT_FILE} >> /combined-certs/combined.pem
+
+echo /opt/mongodb-bi-linux-x86_64-debian92-v${BI_CONNECTOR_VERSION}/bin/mongosqld --sslMode ${SSL_MODE} --sslPemKeyFile /combined-certs/combined --sslPEMKeyPassword ${SSL_KEY_PASS} --sslAllowInvalidCertificates --sslAllowInvalidHostnames --addr ${ADDR} --mongo-uri ${MONGO_URI} --auth --mongo-username ${SAMPLING_USER} --mongo-password ${SAMPLING_PASS} --logPath=mdb-bi-connector.log
+/opt/mongodb-bi-linux-x86_64-debian92-v${BI_CONNECTOR_VERSION}/bin/mongosqld --sslMode ${SSL_MODE} --sslPemKeyFile /combined-certs/combined --sslPEMKeyPassword ${SSL_KEY_PASS} --sslAllowInvalidCertificates --sslAllowInvalidHostnames --addr ${ADDR} --mongo-uri ${MONGO_URI} --auth --mongo-username ${SAMPLING_USER} --mongo-password ${SAMPLING_PASS} --logPath=mdb-bi-connector.log  --logAppend --logRotate reopen -vv
 
