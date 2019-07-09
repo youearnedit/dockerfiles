09ef5cbc2ece5593b97feb3eb3ebf00a3b243667
John Davis <john.davis@youearnedit.com>
2019-07-09 10:27:17 -0500

    Cleanup the run script a little

--- mdb-bi-connector/bin/run.sh
+++ mdb-bi-connector/bin/run.sh
@@ -9,12 +9,8 @@ echo "starting connector application"
 #Concatenate the cert and key together
 cat ${SSL_CERT_FILE} >> /combined.pem
 cat ${SSL_KEY_FILE} >> /combined.pem
-cat /combined.pem
+
 /opt/mongodb-bi-linux-x86_64-debian92-v${BI_CONNECTOR_VERSION}/bin/mongosqld --addr ${ADDR} --sslMode ${SSL_MODE} --sslPEMKeyFile /combined.pem --mongo-uri ${MONGO_URI} --auth --mongo-username ${SAMPLING_USER} --mongo-password ${SAMPLING_PASS} --logPath=mdb-bi-connector.log  --logAppend --logRotate reopen -vv &
 tail -f mdb-bi-connector.log&
-
-
-
-echo /opt/mongodb-bi-linux-x86_64-debian92-v${BI_CONNECTOR_VERSION}/bin/mongosqld --sslMode ${SSL_MODE} --sslPemKeyFile /combined.pem --sslAllowInvalidCertificates --sslAllowInvalidHostnames --addr ${ADDR} --mongo-uri ${MONGO_URI} --auth --mongo-username ${SAMPLING_USER} --mongo-password ${SAMPLING_PASS} --logPath=mdb-bi-connector.log
 
 fg %1
