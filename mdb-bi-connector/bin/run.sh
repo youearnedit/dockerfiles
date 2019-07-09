e0dab211892305095333570f3e5babeb2412036c
John Davis <john.davis@youearnedit.com>
2019-07-09 09:08:37 -0500

    Add some logging

--- mdb-bi-connector/bin/run.sh
+++ mdb-bi-connector/bin/run.sh
@@ -5,11 +5,14 @@ set -m
 MONGO_URI="mongodb://${MONGO_HOST}:${MONGO_PORT}"
 service rsyslog start
 mkfifo mdb-bi-connector.log
-/opt/mongodb-bi-linux-x86_64-debian92-v${BI_CONNECTOR_VERSION}/bin/mongosqld --sslMode ${SSL_MODE} --sslPemKeyFile /combined.pem --sslAllowInvalidCertificates --sslAllowInvalidHostnames --addr ${ADDR} --mongo-uri ${MONGO_URI} --auth --mongo-username ${SAMPLING_USER} --mongo-password ${SAMPLING_PASS} --logPath=mdb-bi-connector.log  --logAppend --logRotate reopen -vv &
-tail -f mdb-bi-connector.log&
+echo "starting connector application"
 #Concatenate the cert and key together
 cat ${SSL_CERT_FILE} >> /combined.pem
 cat ${SSL_KEY_FILE} >> /combined.pem
+cat /combined.pem
+/opt/mongodb-bi-linux-x86_64-debian92-v${BI_CONNECTOR_VERSION}/bin/mongosqld --sslMode ${SSL_MODE} --sslPemKeyFile /combined.pem --sslAllowInvalidCertificates --sslAllowInvalidHostnames --addr ${ADDR} --mongo-uri ${MONGO_URI} --auth --mongo-username ${SAMPLING_USER} --mongo-password ${SAMPLING_PASS} --logPath=mdb-bi-connector.log  --logAppend --logRotate reopen -vv &
+tail -f mdb-bi-connector.log&
+
 
 
 echo /opt/mongodb-bi-linux-x86_64-debian92-v${BI_CONNECTOR_VERSION}/bin/mongosqld --sslMode ${SSL_MODE} --sslPemKeyFile /combined.pem --sslAllowInvalidCertificates --sslAllowInvalidHostnames --addr ${ADDR} --mongo-uri ${MONGO_URI} --auth --mongo-username ${SAMPLING_USER} --mongo-password ${SAMPLING_PASS} --logPath=mdb-bi-connector.log
