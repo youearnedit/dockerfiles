5c6ddbeee641e58e7e221b813cd28dec45f5f603
John Davis <john.davis@youearnedit.com>
2019-07-11 09:07:17 -0500

    Echo URI

--- mdb-bi-connector/bin/run.sh
+++ mdb-bi-connector/bin/run.sh
@@ -9,6 +9,7 @@ echo "starting connector application"
 cat ${SSL_CERT_FILE} >> /combined.pem
 cat ${SSL_KEY_FILE} >> /combined.pem
 
+echo "$MONGO_URI"
 /opt/mongodb-bi-linux-x86_64-debian92-v${BI_CONNECTOR_VERSION}/bin/mongosqld --addr ${ADDR} --sslMode ${SSL_MODE} --sslPEMKeyFile /combined.pem --mongo-uri ${MONGO_URI} --auth --mongo-username ${SAMPLING_USER} --mongo-password ${SAMPLING_PASS} --logPath=mdb-bi-connector.log  --logAppend --logRotate reopen -vv ${EXTRA_OPTS} &
 tail -f mdb-bi-connector.log&
 
